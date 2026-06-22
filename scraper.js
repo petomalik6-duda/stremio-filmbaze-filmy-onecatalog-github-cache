5fad46f7e8f5b03bf668912bdd3541bfc6eddf0d
petomalik6-duda <petomalik6@gmail.com>
2026-06-22 21:37:30 +0200

    Delete scraper.js

--- scraper.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import { refreshCache } from './src/catalog.js';
-
-const metas = await refreshCache();
-console.log(JSON.stringify({ items: metas.length, sample: metas.slice(0, 5) }, null, 2));
