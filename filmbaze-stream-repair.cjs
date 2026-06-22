720f4022f3bf66022336a404dab870f37a29a6ef
petomalik6-duda <petomalik6@gmail.com>
2026-06-22 21:39:20 +0200

    Delete filmbaze-stream-repair.cjs

--- filmbaze-stream-repair.cjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-// Safe placeholder repair. It does not delete or overwrite working values.
-// It only marks primaryVideo:null + detailChecked:true movies for retry.
-require('./force-repair-primaryvideo-null.cjs');
